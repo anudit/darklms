95ed89de3cfeebe54234962afdb9f8e018991244
Anudit Nagar <nagaranudit@gmail.com>
2019-04-26 00:35:22 +0530

    Add Tapermonkey Script

--- index.py
+++
@@ -1,5 +0,0 @@
-from git import Repo
-from tqdm import tqdm
-
-repo = Repo('./')
-repo.index.commit("Fix Footer");
---
+++ tapermonkey.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Dark LMS
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Try to make the LMS less of an eyesore
+// @author       NikRamakrishnan
+// @match        *://lms.bennett.edu.in/*
+// @grant        none
+// @run-at      document-start
+// ==/UserScript==
+
+window.addEventListener('DOMContentLoaded', function (e) {
+	console.log('DOM loaded, processing stuff...');
+	if (window.addCss !== undefined) {
+		window.addCss();
+	} else {
+		var ss = document.createElement('link');
+		ss.rel = "stylesheet";
+		ss.type = "text/css";
+		ss.href = "https://gitcdn.link/repo/anuditnagar/darklms/master/darklms.css";
+		document.head.appendChild(ss);
+	}
+});s
